c05b530cff52963ca0a5eddae417404783e1da8b
jlearnph <64955404+jlearnph@users.noreply.github.com>
2020-09-04 11:13:13 +0800

    Update youtube_downloader.py

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -14,7 +14,7 @@ layout = [[sg.Text('Enter video / playlist link: ')],
           [sg.Text('Downloading:'), sg.Text(size=(15,1), key='-OUTPUT-')]
           ]      
 
-window = sg.Window('YouTube Downloader by Lester', layout)      
+window = sg.Window('YouTube Downloader by JLearn PH', layout)      
 
 while True:                         
     event, values = window.read()
@@ -24,31 +24,38 @@ while True:
 
     #for a playlist
     if event == 'Download':
-        try:
-            playlist = Playlist(link)
-            playlist._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
-            show += f'\nStarting download for {len(playlist.video_urls)} videos\n'
-            count = 1
-            for url in playlist.video_urls:
-                try:
-                    yt = YouTube(link)
-                    yt.streams.first().download(download_folder)
-                    show += f'\n[+] Success {count}: {yt.title}'
-                except:
-                    show += f'\n[+] Failed {count}: {yt.title}'
-                count +=1
-                window['-OUTPUT-'].update(f'{count-1}/{len(playlist.video_urls)}')
+        if (link == '' and download_folder == ''):
+            sg.popup_error('Enter a YouTube Link and Download Folder')
+        elif link == '':
+            sg.popup_error('Enter a YouTube Link')
+        elif download_folder == '':
+            sg.popup_error('Select Download Folder')
+        else:
+            try:
+                playlist = Playlist(link)
+                playlist._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
+                show += f'\nStarting download for {len(playlist.video_urls)} videos\n'
+                count = 1
+                for url in playlist.video_urls:
+                    try:
+                        yt = YouTube(link)
+                        yt.streams.first().download(download_folder)
+                        show += f'\n[+] Success {count}: {yt.title}'
+                    except:
+                        show += f'\n[+] Failed {count}: {yt.title}'
+                    count +=1
+                    window['-OUTPUT-'].update(f'{count-1}/{len(playlist.video_urls)}')
 
-        #for only 1 video
-        except:
-            show += '\nStarting download for 1 video\n'
-            show += f'\n[+] Video: {link}'
-            YouTube(link).streams.first().download(download_folder)
-        
-        sg.popup_scrolled(f'{show}\n\nFinished Downloading\n', size=(100,20))
+            #for only 1 video
+            except:
+                show += '\nStarting download for 1 video\n'
+                show += f'\n[+] Video: {link}'
+                YouTube(link).streams.first().download(download_folder)
+            
+            sg.popup_scrolled(f'{show}\n\nFinished Downloading\n', size=(100,20))
         
 
-    if event == sg.WIN_CLOSED or event == 'Exit':
+    if (event == sg.WIN_CLOSED or event == 'Exit'):
         break      
 
 window.close()
